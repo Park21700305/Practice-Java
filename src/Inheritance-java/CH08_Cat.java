bf36b90273357315381791f47bb1edd928d72bf4
Park21700305 <93187535+Park21700305@users.noreply.github.com>
2022-05-11 10:52:50 +0900

    create Inheritance-java

---
+++ src/Inheritance-java/CH08_Cat.java
@@ -0,0 +1,6 @@
+//21700305 박지성
+public class CH08_Cat extends CH08_Animal {
+    CH08_Cat() {
+        food = "(Cat!)Fish";
+    }
+}
---
+++ src/Inheritance-java/CH08_Dog.java
@@ -0,0 +1,6 @@
+//21700305 박지성
+public class CH08_Dog extends CH08_Animal{
+    CH08_Dog() {
+        food = "(Dog!)Sausage";
+    }
+}
